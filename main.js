f24c06442cf03a8648dff53f4cf8116887055480
isabellamychal02 <87705229+isabellamychal02@users.noreply.github.com>
2021-07-27 15:53:21 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var names = ["mom", "dad", "bella", "grandma", "grandpa"];
+console.log(names);
+document.getElementById("names").innerHTML = names;
+var family_1 = names[0];
+console.log(family_1);
+var family_two = names[1];
+console.log(family_two);
+var family_3 = names[2];
+console.log(family_3);
+var family_4 = names[3];
+console.log(family-4);
+var family_5 = names[4];
+console.log(family_5)
+var length_of_array = names.length
+console.log(length_of_array);
